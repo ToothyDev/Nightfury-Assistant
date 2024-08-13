287cfbf3ff1dfa5f46a3a7438de0e3315e0ee8fc
ToothyDev <chosenfate@gmx.de>
2024-08-13 17:36:22 +0200

    Color the avatar embed according to user color

--- cogs/user_commands.py
+++ cogs/user_commands.py
@@ -10,7 +10,8 @@ class UserCommands(discord.Cog, name="user_commands"):
 
     @user_command()
     async def avatar(self, ctx: discord.ApplicationContext, member: discord.Member):
-        embed = discord.Embed(color=Colors.tailfin)
+        user = await self.bot.fetch_user(member.id)  # Color attribute is only available via fetch
+        embed = discord.Embed(color=user.accent_color)
         embed.set_image(url=member.avatar.with_size(4096))
         embed.set_footer(text="Nightfury Assistant", icon_url=self.bot.user.avatar.url)
         await ctx.respond(embed=embed, ephemeral=True)
