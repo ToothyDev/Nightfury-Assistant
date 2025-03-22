f14dd0287da2c0cadcdde39a88038adbff6944fc
ToothyDev <chosenfate@gmx.de>
2025-03-22 22:27:11 +0100

    Make embed command reflect all embed possibilities (at least 95%)

--- cogs/slash_commands.py
+++ cogs/slash_commands.py
@@ -1,3 +1,5 @@
+import datetime
+
 import discord
 from discord import slash_command, option
 
@@ -15,11 +17,20 @@ class SlashCommands(discord.Cog, name="slash_commands"):
             default=discord.Color.default())
     @option("image", discord.Attachment, description="The image of the embed", required=False)
     @option("thumbnail", bool, description="Make the image the thumbnail?", required=False, default=False)
+    @option("author", description="Set the author name", required=False)
+    @option("author_url", description="Set the author url", required=False)
+    @option("author_icon_url", description="Set the author icon url", required=False)
+    @option("footer", description="Set the footer text", required=False)
+    @option("footer_icon_url", description="Set the footer icon url", required=False)
+    @option("url", description="The url of the embed", required=False)
+    @option("timestamp", float, description="The timestamp of the embed", required=False)
     @option("field1", description="Title|Content of field 1", required=False)
     @option("field2", description="Title|Content of field 2", required=False)
     @option("field3", description="Title|Content of field 3", required=False)
     async def embed(self, ctx: discord.ApplicationContext, description: str, title: str, image: discord.Attachment,
-                    thumbnail: bool, field1: str, field2: str, field3: str, color: discord.Color):
+                    thumbnail: bool, author: str, author_url: str, author_icon_url: str, footer: str,
+                    footer_icon_url: str, url: str, timestamp: float, field1: str, field2: str,
+                    field3: str, color: discord.Color):
         embed = discord.Embed(title=title, description=description, color=color)
         if image:
             if thumbnail:
@@ -27,11 +38,23 @@ class SlashCommands(discord.Cog, name="slash_commands"):
             else:
                 embed.set_image(url=image.url)
 
+        if author:
+            embed.set_author(name=author, url=author_url, icon_url=author_icon_url)
+
+        if footer:
+            embed.set_footer(text=footer, icon_url=footer_icon_url)
+
+        if url:
+            embed.url = url
+
+        if timestamp:
+            embed.timestamp = datetime.datetime.fromtimestamp(timestamp)
+
         for field in [field1, field2, field3]:
             if field:
                 embed.add_field(name=field.split("|")[0], value=field.split("|")[1])
 
-        await ctx.respond(embed=embed)
+        await ctx.respond(embed=embed, ephemeral=True)
 
 
 def setup(bot):
