9e2643fe3772c3d163b8c05adf8ff81d5b8482bd
ToothyDev <chosenfate@gmx.de>
2025-08-11 15:29:35 +0200

    Update example.config.py

--- example.config.py
+++ example.config.py
@@ -1,1 +1,9 @@
 token = "DISCORD BOT TOKEN"
+
+llm_api_key = "API key for the LLM API"
+llm_base_url = "for example https://api.groq.com/openai/v1/"
+llm_model_name = "name of the model at your provider"
+
+emojis = {
+    "clean emoji name": "emoji markdown"
+}
